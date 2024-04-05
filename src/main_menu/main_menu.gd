c6f062c71dec5f846fa8b265c6dc8fca14e2d75c
serb2331 <serban.foica@gmail.com>
2024-04-05 23:13:38 +0300

    add base main menu

---
+++ src/main_menu/main_menu.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+
+
+func _on_button_start_pressed():
+	# move to start scene
+	pass # Replace with function body.
+
+
+func _on_button_toggle_sound_pressed():
+	pass # Replace with function body.
+
+func _on_button_exit_pressed():
+	get_tree().quit()
