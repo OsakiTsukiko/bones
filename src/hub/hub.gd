6ba58dcad80671e168ff3813516afe9e8f078b11
PotatoGolden76 <robert.harangus@gmail.com>
2024-04-06 15:15:18 +0300

    boss room added

--- src/hub/hub.gd
+++ src/hub/hub.gd
@@ -4,8 +4,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	var room: Room = Room.new(Vector2i(0, 0));
+	var room: Room = Room.new(Vector2i(0, 0), false);
 	var room_3d: Room3D = Room3D.new(room)
 
 	add_child(room_3d);
