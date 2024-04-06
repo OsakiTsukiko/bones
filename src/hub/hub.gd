9e1d506734ffeccf3ad7c6b921dfa3205e4d0672
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2024-04-06 22:06:32 +0300

    bruv

--- src/hub/hub.gd
+++ src/hub/hub.gd
@@ -11,7 +11,7 @@ func load_data(data: Dictionary):
 
 func setup_room():
 	var room: Room = Room.new(Vector2i(0, 0), false);
-	var room_3d: Room3D = Room3D.new(room)
+	var room_3d: Room3D = Room3D.new(room, [])
 	add_child(room_3d);
 
 # Called when the node enters the scene tree for the first time.
@@ -29,7 +29,7 @@ func _process(delta):
 func _on_portal_body_entered(body):
 	print(1, body)
 	if body == $Player:
-		call_deferred("_change_scene_to", test_scene)
+		call_deferred("ed_wrapper")
 
-func _change_scene_to(scene: PackedScene):
-	get_tree().change_scene_to_packed(scene)
+func ed_wrapper():
+	SceneManager.enter_dungeon()
--- src/test/Node3D.gd
+++ src/test/Node3D.gd
@@ -14,13 +14,18 @@ func _ready():
 		r = dgn.rooms[rkey]
 		break
 	 
-	var idk = Room3D.new(r)
+	var idk = Room3D.new(r, [])
 	blks.add_child(idk)
 	
 	var dog = dog_scene.instantiate()
 	dog.init($Player)
 	dog.position = Vector3(14, 1, 14)
 	add_child(dog)
+	
+	var dog2 = dog_scene.instantiate()
+	dog2.init($Player)
+	dog2.position = Vector3(15, 1, 14)
+	add_child(dog2)
 	
 	$GroundItem._init_instance(0.3)
 
