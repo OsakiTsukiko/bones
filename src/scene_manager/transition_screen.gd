74d17f9b517b753b7ae0f12aa66f3a11c0d66c8b
PotatoGolden76 <robert.harangus@gmail.com>
2024-04-06 17:36:48 +0300

    done

--- src/scene_manager/transition_screen.gd
+++ src/scene_manager/transition_screen.gd
@@ -2,13 +2,8 @@ extends CanvasLayer
 
 signal done_fading
 
-func _ready():
-	transition_in()
-
 func transition_in():
-	
 	$AnimationPlayer.play("fade_out")
-
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fade_out":
