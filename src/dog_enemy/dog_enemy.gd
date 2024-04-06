b43e2172c2d81bca8ac273c6fa7e5b0c2d0bcfc9
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2024-04-06 06:50:16 +0300

    bites

--- src/dog_enemy/dog_enemy.gd
+++ src/dog_enemy/dog_enemy.gd
@@ -7,23 +7,43 @@ const SPEED = 100  # Adjust the speed as needed
 
 var player: Node3D
 
+var is_attacking: bool
+
 func init(p_player: Node3D) -> void:
 	player = p_player
 
 func _physics_process(delta):
-	if position.x > player.position.x - .2 and position.x < player.position.x + .2 and position.y > player.position.y - .2 and position.y < player.position.y + .2:
-		return
-	look_at(player.position);
-	
-	var direction = Vector3.FORWARD;
-	if (direction != Vector3.ZERO):
-		# anim_player.current_animation = "run"
-		anim_sprite.animation = "run"
-	
-	direction = direction.normalized()
-	velocity = transform.basis * direction * delta * SPEED;
-	
-	move_and_slide();
+	#if position.x > player.position.x - .2 and position.x < player.position.x + .2 and position.y > player.position.y - .2 and position.y < player.position.y + .2:
+		#return
+	if (!is_attacking):
+		
+		look_at(player.position);
+		
+		var direction = Vector3.FORWARD;
+		if (direction != Vector3.ZERO):
+			# anim_player.current_animation = "run"
+			anim_sprite.animation = "run"
+		
+		direction = direction.normalized()
+		velocity = transform.basis * direction * delta * SPEED;
+		
+		move_and_slide();
 
 func flip(val: bool):
 	anim_sprite.flip_h = val
+
+func attack():
+	is_attacking = true
+	do_attack()
+
+func stop_attack():
+	is_attacking = false
+
+func do_attack():
+	anim_sprite.animation = "attack"
+	$AnimAttackTimer.start()
+
+func _on_anim_attack_timer_timeout() -> void:
+	print("TODO: DEAL DAMAGE")
+	if (is_attacking):
+		do_attack()
