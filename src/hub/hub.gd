1f54a6104f903cb66319f214f7ae9fee86fc1677
serb2331 <serban.foica@gmail.com>
2024-04-06 08:18:30 +0300

    add portal to test

--- src/hub/hub.gd
+++ src/hub/hub.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var test_scene: PackedScene = preload("res://src/test/test.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,9 +9,11 @@ func _ready():
 	var room_3d: Room3D = Room3D.new(room)
 
 	add_child(room_3d);
-	Room.print_room_layout(room);
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_portal_body_entered(body):
+	if body == $Player:
+		get_tree().change_scene_to_packed(test_scene)
