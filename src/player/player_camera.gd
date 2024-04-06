3ba296cc7f9763356a69064c087ce5870c6ded64
PotatoGolden76 <robert.harangus@gmail.com>
2024-04-07 02:41:28 +0300

    juice

---
+++ src/player/player_camera.gd
@@ -0,0 +1,21 @@
+extends Camera3D
+
+@export var period = 0.3
+@export var magnitude = 0.05
+
+func camera_shake():
+	var initial_transform = self.transform 
+	var elapsed_time = 0.0
+
+	while elapsed_time < period:
+		var offset = Vector3(
+			randf_range(-magnitude, magnitude),
+			randf_range(-magnitude, magnitude),
+			0.0
+		)
+
+		self.transform.origin = initial_transform.origin + offset
+		elapsed_time += get_process_delta_time()
+		await get_tree().process_frame
+
+	self.transform = initial_transform
