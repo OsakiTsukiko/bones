3730745d886bfafdde474155c927827ad688e6a6
serb2331 <serban.foica@gmail.com>
2024-04-06 11:44:59 +0300

    add ground_objects and shop keeper

---
+++ src/ground_object/ground_object.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+func _init_instance(collision_radius: float, object_texture: Texture2D = null):
+	var collision_shape: CylinderShape3D = $CollisionShape3D.shape
+	var sprite = $Sprite3D
+	collision_shape.radius = collision_radius
+	if object_texture != null:
+		sprite.texture = object_texture
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
